fdc2afd984fc43bfd7ed0dfb002aab06318c108f
Mawuli ADJODA <koffimawulli.adjoda@gmail.com>
2023-12-23 07:46:56 +0100

    init project

---
+++ src/test/java/com/esprit/springbootcicd/SpringBootCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.springbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
